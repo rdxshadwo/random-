f5d4c0947aee4339ad813decb3f1cfa10e4bc206
RDX-SHADWO <mdsajim01321411039@gmail.com>
2024-09-25 15:22:11 +0600

    Create random.py
    apt update 
    apt upgrade -y
    pkg install git
    pkg install python
    git clone https://github.com/rdxshadwo/random.git
    cd random
    python random.py

---
+++ random.py
@@ -0,0 +1,13 @@
+import os,random,string
+while True:
+  try:
+   folder_name =''.join(random.choices(string.ascii_lowercase,k=10))
+   folder_path =os.path.join("/sdcard",folder_name)
+   os.makedirs(folder_path,exist_ok =True)
+  except:
+    pass
+    
+    #--------------------------------#
+    #DEVELOPER : JR RDX SHADWO #
+    #----------------------------------#
+    
